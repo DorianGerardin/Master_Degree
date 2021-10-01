9ed7981b6306179d2a707ca1d5132204068060b4
DorianGerardin <gryfadon@gmail.com>
2021-10-01 14:29:05 +0200

    fin ex9

--- M1/s7/HAI720I_progEfficace/TP1/partie2.cpp
+++ M1/s7/HAI720I_progEfficace/TP1/partie2.cpp
@@ -29,24 +29,25 @@ int reduce4(std::vector<int> arr) {
 
 }
 
-int reduceSIMD(std::vector<int> arr) {
+double reduceSIMD(std::vector<double> arr) {
 
 	__m256d res = _mm256_set1_pd(1);
-	int trueResult[4];
+	double trueResult[4];
 	int size = arr.size();
 	__m256d aux;
 	for (int i = 0; i < size-3; i+=4)
 	{
 		aux = _mm256_load_pd(arr.data() + i);
-		res = _mm256_mul_pd(res, aux);
+		res = _mm256_add_pd(res, aux);
 	}
-	_mm256_store_pd(trueResult);
-	return trueResult[0]*trueResult[1]*trueResult[2]*trueResult[3];
+	_mm256_store_pd(trueResult, res);
+	return trueResult[0]+trueResult[1]+trueResult[2]+trueResult[3];
 
 }
 
 int main(int argc, char **argv) {
-	return 0;
+	std::vector<double> v = {1,2,3,4,5,6,7,8,9,10,11,12};
+	printf("result : %f\n", reduceSIMD(v));
 }
 
 
