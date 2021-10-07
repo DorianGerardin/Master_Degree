9d5a4fb87b51940ec9a93ef9d8c420b8f831a7f4
DorianGerardin <gryfadon@gmail.com>
2021-10-07 15:14:14 +0200

    correction SIMD

--- M1/s7/HAI720I_progEfficace/TP1/partie2.cpp
+++ M1/s7/HAI720I_progEfficace/TP1/partie2.cpp
@@ -2,28 +2,28 @@
 #include <iostream>
 #include <vector>
 
-int reduce(std::vector<int> arr) {
+double reduce(std::vector<double> arr) {
 
-	int res = 1;
+	double res = 0;
 	int size = arr.size();
 	for (int i = 0; i < size; ++i)
 	{
-		res *= arr[i];
+		res += arr[i];
 	}
 	return res;
 
 }
 
-int reduce4(std::vector<int> arr) {
+double reduce4(std::vector<double> arr) {
 
-	int res = 1;
+	double res = 0;
 	int size = arr.size();
 	for (int i = 0; i < size-3; i+=4)
 	{
-		res *= arr[i];
-		res *= arr[i+1];
-		res *= arr[i+2];
-		res *= arr[i+3];
+		res += arr[i];
+		res += arr[i+1];
+		res += arr[i+2];
+		res += arr[i+3];
 	}
 	return res;
 
@@ -31,7 +31,7 @@ int reduce4(std::vector<int> arr) {
 
 double reduceSIMD(std::vector<double> arr) {
 
-	__m256d res = _mm256_set1_pd(1);
+	__m256d res = _mm256_set1_pd(0);
 	double trueResult[4];
 	int size = arr.size();
 	__m256d aux;
@@ -47,7 +47,9 @@ double reduceSIMD(std::vector<double> arr) {
 
 int main(int argc, char **argv) {
 	std::vector<double> v = {1,2,3,4,5,6,7,8,9,10,11,12};
-	printf("result : %f\n", reduceSIMD(v));
+	printf("result SIMD: %f\n", reduceSIMD(v));
+	printf("result reduce: %f\n", reduce(v));
+	printf("result reduce 4 : %f\n", reduce4(v));
 }
 
 
