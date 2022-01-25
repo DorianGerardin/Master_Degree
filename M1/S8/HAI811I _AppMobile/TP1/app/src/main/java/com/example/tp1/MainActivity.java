35a461254699e82e3347c30ae2629f08a2aaacab
DorianGerardin <gryfadon@gmail.com>
2022-01-25 16:21:10 +0100

    td1

---
+++ M1/S8/HAI811I _AppMobile/TP1/app/src/main/java/com/example/tp1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.tp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(R.layout.layout3);
+    }
+}
