dc34287a3199562432ab7e9bdb8aba65978832f7
DorianGerardin <gryfadon@gmail.com>
2022-01-27 12:57:21 +0100

    debut tp

---
+++ M1/s8/HAI819I_MoteurJeux/TP1/TP1/vertex_shader.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+
+// Input vertex data, different for all executions of this shader.
+layout(location = 0) in vec3 vertices_position_modelspace;
+
+//TODO create uniform transformations matrices Model View Projection
+// Values that stay constant for the whole mesh.
+
+uniform mat4 transfo_mat;
+
+void main(){
+
+        // TODO : Output position of the vertex, in clip space : MVP * position
+        gl_Position = transfo_mat * vec4(vertices_position_modelspace,1);
+
+}
+
