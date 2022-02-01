8097b0a0d934cbc9942cf6efcbbfe4512046790b
DorianGerardin <gryfadon@gmail.com>
2022-02-01 16:34:47 +0100

    ex5 en cours

---
+++ M1/s8/HAI811I _AppMobile/TP1/app/src/main/java/com/example/tp1/ex3.java
@@ -0,0 +1,16 @@
+package com.example.tp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tp1.R;
+
+public class ex3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex3);
+    }
+}
