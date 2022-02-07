7cf151ac40db2b8d1830239ae389ac5e81b9f093
DorianGerardin <gryfadon@gmail.com>
2022-02-07 21:38:11 +0100

    explo

--- M1/s8/HAI801I_algo_exploration/BFS_DFS/MissCann.js
+++ M1/s8/HAI801I_algo_exploration/BFS_DFS/MissCann.js
@@ -1,16 +1,185 @@
-const initialState = [3, 3, true]
-const finalState = [0, 0, false]
+var inputValue = null
+console.log("inputValue", inputValue)
+var nbPeople = null
+console.log("nbPeople", nbPeople)
+var initialState = null
+var finalState = null
 
-function buildGraph(state) {
-	let label = '<' + state.join('-') + '>'
-	//1 missionnaire en moins
-	let childState1[3]
-	childState1[0] = state[0]-1
-	childState1[1] = state[1]
+function set(value) {
+	inputValue = value;
+	console.log("inputValue", inputValue)
+	nbPeople = parseInt(inputValue);
+	console.log("nbPeople", nbPeople)
+	initialState = [nbPeople, nbPeople, true]
+	finalState = [0, 0, false]
+}
 
-	 
+function buildGraph(state, node, nodeList) {
+	console.log("state actuel : " + state)
+	if(nodeList.map(nodeFromList => nodeFromList.id).includes(node.id)) {
+		console.log("je suis un node deja present")
+		let sameNode = nodeList.filter(nodeFromList => nodeFromList.id === node.id)[0]
+		sameNode.children.forEach(child => {
+			node.appendChild(child)
+		})
+	}
+	else{
+		nodeList.push(node)
+		console.log("je push le node")
+		let childState1 = []; let childState2 = []; let childState3 = []; let childState4 = []; let childState5 = []
+		let label1, label2, label3, label4, label5
+		//Si le bateau est sur la rive de départ
+		if(state[2]) {
+			//1 missionnaire en moins
+			childState1[0] = state[0]-1
+			childState1[1] = state[1]
+			childState1[2] = !state[2]
+			if(respectRule(childState1)) {
+				console.log("\tstate enfant1 : " + childState1)
+				let label1 = '<' + childState1.join('-') + '>'
+				let childNode1 = new Node(label1)
+				node.appendChild(childNode1)
+				buildGraph(childState1, childNode1, nodeList)
+			}
+
+			//2 missionnaire en moins
+			childState2[0] = state[0]-2
+			childState2[1] = state[1]
+			childState2[2] = !state[2]
+			if(respectRule(childState2)) {
+				console.log("\tstate enfant2 : " + childState2)
+				let label2 = '<' + childState2.join('-') + '>'
+				let childNode2 = new Node(label2)
+				node.appendChild(childNode2)
+				buildGraph(childState2, childNode2, nodeList)
+			}
+
+			//1 missionnaire et 1 cannibale en moins
+			childState3[0] = state[0]-1
+			childState3[1] = state[1]-1
+			childState3[2] = !state[2]
+			if(respectRule(childState3)) {
+				console.log("\tstate enfant3 : " + childState3)
+				let label3 = '<' + childState3.join('-') + '>'
+				let childNode3 = new Node(label3)
+				node.appendChild(childNode3)
+				buildGraph(childState3, childNode3, nodeList)
+			}
+
+			//1 cannibale en moins
+			childState4[0] = state[0]
+			childState4[1] = state[1]-1
+			childState4[2] = !state[2]
+			if(respectRule(childState4)) {
+				console.log("\tstate enfant4 : " + childState4)
+				let label4 = '<' + childState4.join('-') + '>'
+				let childNode4 = new Node(label4)
+				node.appendChild(childNode4)
+				buildGraph(childState4, childNode4, nodeList)
+			}
+
+			//2 cannibale en moins
+			childState5[0] = state[0]
+			childState5[1] = state[1]-2
+			childState5[2] = !state[2]
+			if(respectRule(childState5)) {
+				console.log("\tstate enfant5 : " + childState5)
+				let label5 = '<' + childState5.join('-') + '>'
+				let childNode5 = new Node(label5)
+				node.appendChild(childNode5)
+				buildGraph(childState5, childNode5, nodeList)
+			}
+		}
+		//Si le bateau est sur la bonne rive
+		if(!state[2]) {
+			//1 missionnaire en plus
+			childState1[0] = state[0]+1
+			childState1[1] = state[1]
+			childState1[2] = !state[2]
+			if(respectRule(childState1)) {
+				console.log("je rajoute un missionnaire")
+				console.log("\tstate enfant1 : " + childState1)
+				let label1 = '<' + childState1.join('-') + '>'
+				let childNode1 = new Node(label1)
+				node.appendChild(childNode1)
+				buildGraph(childState1, childNode1, nodeList)
+			}
+
+			//2 missionnaire en plus
+			childState2[0] = state[0]+2
+			childState2[1] = state[1]
+			childState2[2] = !state[2]
+			if(respectRule(childState2)) {
+				console.log("\tstate enfant2 : " + childState2)
+				let label2 = '<' + childState2.join('-') + '>'
+				let childNode2 = new Node(label2)
+				node.appendChild(childNode2)
+				buildGraph(childState2, childNode2, nodeList)
+			}
+
+			//1 missionnaire et 1 cannibale en plus
+			childState3[0] = state[0]+1
+			childState3[1] = state[1]+1
+			childState3[2] = !state[2]
+			if(respectRule(childState3)) {
+				console.log("\tstate enfant3 : " + childState3)
+				let label3 = '<' + childState3.join('-') + '>'
+				let childNode3 = new Node(label3)
+				node.appendChild(childNode3)
+				buildGraph(childState3, childNode3, nodeList)
+			}
+
+			//1 cannibale en plus
+			childState4[0] = state[0]
+			childState4[1] = state[1]+1
+			childState4[2] = !state[2]
+			if(respectRule(childState4)) {
+				console.log("\tstate enfant4 : " + childState4)
+				let label4 = '<' + childState4.join('-') + '>'
+				let childNode4 = new Node(label4)
+				node.appendChild(childNode4)
+				buildGraph(childState4, childNode4, nodeList)
+			}
+
+			//2 cannibale en plus
+			childState5[0] = state[0]
+			childState5[1] = state[1]+2
+			childState5[2] = !state[2]
+			if(respectRule(childState5)) {
+				console.log("\tstate enfant5 : " + childState5)
+				let label5 = '<' + childState5.join('-') + '>'
+				let childNode5 = new Node(label5)
+				node.appendChild(childNode5)
+				buildGraph(childState5, childNode5, nodeList)
+			}
+		}
+		return new Graph(nodeList)
+	}
 }
 
 function respectRule(state) {
-	return state[0] <= state[1]
+	console.log(state + " respecte ? : ")
+	if (state[0] > nbPeople || state[0] < 0) {
+		console.log("false"); return false
+	}
+	if (state[1] > nbPeople || state[1] < 0) {
+		console.log("false"); return false
+	}
+	if(state[0] < state[1] && state[0] == 0){ console.log("true"); return true
+	}
+	if (state[0] < state[1]) {
+		console.log("false"); return false
+	}
+	if((nbPeople - state[0] < nbPeople - state[1]) && (nbPeople - state[0] == 0)){ console.log("true"); return true
+	}
+	if(nbPeople - state[0] < nbPeople - state[1] && state[0] > 0) {
+		console.log("false"); return false
+	}
+	console.log("true")
+	return true
 }
+
+/*let nodeList = []
+let node = new Node("<3-3-"+true+">")
+let graph = buildGraph(initialState, node, nodeList)*/
+//graph.nodeList.forEach(node => node.drawChildren())
--- M1/s8/HAI801I_algo_exploration/BFS_DFS/script.js
+++ M1/s8/HAI801I_algo_exploration/BFS_DFS/script.js
@@ -1,9 +1,87 @@
+/*var my_chart = null;
+var simple_chart_config = null;
+var firstNodeLabel = "<3-3-"+true+">"
+
+
+function start() {
+	simple_chart_config = {
+        chart: {
+            container: "#tree-simple",
+            connectors: {
+                type : "straight"
+            },
+            scrollbar: "fancy",
+            animation: {
+                connectorsAnimation : "linear",
+                nodeAnimation : "linear",
+            },
+            node : {
+                collapsable: false,
+            }
+        },
+        
+        nodeStructure: {
+            innerHTML : "<span>" + firstNodeLabel + "</span>",
+            HTMLid : firstNodeLabel,
+            collapsable : false,
+        }
+    };
+
+    document.body.innerHTML = "<div id='tree-simple' style='width:100%; height: 60%;'> </div>";
+    my_chart = new Treant(simple_chart_config);
+}
+
+function getNodeStructure(id) {
+    let spanString = "<span>" + id + "</span>"
+
+    let jsonNODE = {
+        HTMLid : id,
+        innerHTML: spanString,
+        collapsable:false
+    }
+
+    return jsonNODE;
+}
+
+function constructTree(graph, node) {
+	start()
+	let markedNodes = new Array()
+	let searchOrder = new Array()
+
+	file = new Array()
+	file.push(node)
+
+	markedNodes.push(node.id)
+
+	while(file.length !== 0) {
+		node = file.shift();
+		searchOrder.push(node.id)
+		node.children.forEach((child) => {
+			if(!markedNodes.includes(child.id)) {
+				file.push(child)
+				markedNodes.push(child.id)
+
+				let parentNode = my_chart.tree.nodeDB.db.find(obj => {
+			        return obj.nodeHTMLid === node.id;
+			    })
+			    console.log("parentNode", parentNode)
+			    let childNode = getNodeStructure(child.id)
+			    console.log("childNode", childNode)
+				let nextNode = my_chart.tree.addNode(parentNode, childNode);
+				console.log("nextNode", nextNode);
+			}
+		})
+	}
+	console.log(simple_chart_config)
+}*/
+
+
 function showOrderBFS(nodeList) {
-	document.getElementById("output-BFS").innerHTML += (nodeList.join('-'))
+	document.getElementById("output-BFS").innerHTML += (nodeList.join(' - '))
 }
 
 function showOrderDFS(nodeList) {
-	document.getElementById("output-DFS").innerHTML += (nodeList.join('-'))
+	document.getElementById("output-DFS").innerHTML += (nodeList.join(' - '))
 }
 
 function BFS(graph, node) {
@@ -53,9 +131,45 @@ function DFS(graph, node, markedNodes) {
 }
 
 
+function showDFS() {
+	set(document.getElementById("input").value)
+	var title = document.getElementById("title"); document.body.removeChild(title);
+	var context = document.getElementById("context"); document.body.removeChild(context);
+	var form = document.getElementById("form"); document.body.removeChild(form);
+	var buttons = document.getElementById("buttons"); document.body.removeChild(buttons);
+	var bfs = document.getElementById("output-BFS"); document.body.removeChild(bfs);
+
+	let nodeList = new Array()
+	let firstNode = new Node("<"+inputValue+"-"+inputValue+"-"+true+">")
+	let graph = buildGraph(initialState, firstNode, nodeList)
+
+	let markedNodes = new Array()
+	DFS(graph, firstNode, markedNodes)
+	showOrderDFS(markedNodes)
+	//constructTree(graph, firstNode)
+
+}
+
+function showBFS() {
+	set(document.getElementById("input").value)
+	var title = document.getElementById("title"); document.body.removeChild(title);
+	var context = document.getElementById("context"); document.body.removeChild(context);
+	var form = document.getElementById("form"); document.body.removeChild(form);
+	var buttons = document.getElementById("buttons"); document.body.removeChild(buttons);
+	var dfs = document.getElementById("output-DFS"); document.body.removeChild(dfs);
+
+	let nodeList = new Array()
+	let firstNode = new Node("<"+inputValue+"-"+inputValue+"-"+true+">")
+	let graph = buildGraph(initialState, firstNode, nodeList)
+
+	let markedNodes = new Array()
+	BFS(graph, firstNode)
+	showOrderBFS(markedNodes)
+}
 
 
-nodeList = [
+
+/*nodeList = [
 nodeA = new Node("A"),
 nodeB = new Node("B"),
 nodeC = new Node("C"),
@@ -71,17 +185,12 @@ nodeE.appendChild(nodeA)
 
 graph = new Graph(nodeList)
 
-/*nodeB.drawChildren()
-nodeC.drawChildren()
-nodeE.drawChildren()
-nodeA.drawChildren()*/
-
 //BFS
 BFS(graph, nodeA)
 
 //DFS
 let markedNodes = new Array()
-DFS(graph, nodeA, markedNodes)
+DFS(graph, nodeA, markedNodes)*/
 
 
 
